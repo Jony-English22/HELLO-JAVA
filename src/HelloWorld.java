2ebe8c99e3df28167ebb6e381ee4c91aa0bfc658
Jony-English22 <jonathanrp200322@gmail.com>
2025-09-15 14:50:18 -0600

    Add Java project structure

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        //Este es un comentario de una linea
+
+        /*
+        Este es un
+        comentario
+        de varias
+        lineas
+         */
+
+        System.out.println("Hola, Java!");
+
+    }
+}
